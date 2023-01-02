eb8c096d25c431a9341967295a84810e300dc7e5
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2023-01-02 23:05:41 +0000

    converting picked locations to human readable addresses

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -12,7 +12,7 @@ import {
 } from "@react-navigation/native";
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colours } from "../../constants/colours";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 
 function LocationPicker({ onPickLocation }) {
   const [pickedLocation, setPickedLocation] = useState();
@@ -35,7 +35,16 @@ function LocationPicker({ onPickLocation }) {
   }, [route, isFocused]);
 
   useEffect(() => {
-    onPickLocation(pickedLocation);
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = await getAddress(
+          pickedLocation.lat,
+          pickedLocation.lng
+        );
+        onPickLocation({ ...pickedLocation, address: address });
+      }
+    }
+    handleLocation();
   }, [pickedLocation, onPickLocation]);
 
   async function verifyPermissions() {
