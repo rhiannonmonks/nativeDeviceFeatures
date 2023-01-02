f1bb1e8a33c89de1000d7c568633e82ba147e755
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2023-01-02 16:15:02 +0000

    enabling location permissions and locaing a user with lat and long in the console

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,9 +1,44 @@
-import { View, StyleSheet } from "react-native";
+import { Alert, View, StyleSheet } from "react-native";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colours } from "../../constants/colours";
 
 function LocationPicker() {
-  function getLocationHandler() {}
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "insufficient Permissions!",
+        "You needs to grant location permissions to use this app."
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
 
   function pickOnMapHandler() {}
 
